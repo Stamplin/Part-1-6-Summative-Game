4ab23a8a2f4ea6c21bd34530b7b714cee42e8c19
qtac <qtac@DESKTOP-2PUT7CT>
2025-03-04 17:12:52 -0500

    done 100%

--- Program.cs
+++ Program.cs
@@ -7,47 +7,10 @@ namespace Part_1_6_Summative_Game
         static void Main(string[] args)
         {
             Die die1, die2;
-
-            //dice casino game
-            Console.WriteLine("                                                                   \r\n ____  _            _____         _            _____               \r\n|    \\|_|___ ___   |     |___ ___|_|___ ___   |   __|___ _____ ___ \r\n|  |  | |  _| -_|  |   --| .'|_ -| |   | . |  |  |  | .'|     | -_|\r\n|____/|_|___|___|  |_____|__,|___|_|_|_|___|  |_____|__,|_|_|_|___|\r\n                                                                   ");
-            Console.WriteLine("You will be rolling two dice, and betting on the ending of the dice. \nIf you pick the correct one you win, else you lose!");
-            Console.WriteLine();
+            die1 = new Die();
+            die2 = new Die();
             //initial balance
-            int balance = 100;
-            
-            //method for dice rolling
-            static void RollDice()
-            {
-                Die die1, die2;
-                die1 = new Die();
-                die2 = new Die();
-                die1.RollDie();
-                die2.RollDie();
-                Console.WriteLine("You rolled a " + die1 + " and a " + die2);
-            }
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            double balance = 100;
 
 
 
@@ -88,36 +51,120 @@ namespace Part_1_6_Summative_Game
                 bool Betting = true;
                 while (Betting)
                 {
+                    Console.WriteLine();
                     //what would you like to bet on?
                     Console.WriteLine("What would you like to bet on?");
+                    Console.WriteLine("==========================");
                     //currentcy of bet
                     Console.WriteLine("You are currenly betting " + bet + "$");
                     Console.WriteLine("You have " + balance + "$ left in your balance.");
+                    Console.WriteLine("==========================");
                     Console.WriteLine();
                     Console.WriteLine("1. Double (If the dice roll the same number, your bet will be doubled!)");
+                    Console.WriteLine();
                     Console.WriteLine("2. Not Double (If the dice does not roll the same number, your will win half of what you bet!)");
+                    Console.WriteLine();
                     Console.WriteLine("3. Even Sum (If the sum of the dice is even, you will win your bet!)");
+                    Console.WriteLine();
                     Console.WriteLine("4. Odd Sum (If the sum of the dice is odd, you will win your bet!)");
 
                     string guess = Console.ReadLine();
                     //double
-                    if (guess == "1") 
+                    if (guess == "1")
                     {
                         Console.Clear();
                         Console.WriteLine("You have selected to bet on a double!");
-                        RollDice();
-                        
+                        RollAndDraw(die1, die2);
+                        Console.WriteLine($"{die1.Roll} and {die2.Roll}");
+                        //checking if double
+                        if (die1.Roll == die2.Roll)
+                        {
+                            Console.WriteLine("You won " + bet * 2 + "$!");
+                            balance += bet * 2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lost " + bet + "$!");
+                        }
+                        Console.WriteLine("Press ENTER to continue.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        Betting = false;
                     }
+                    //not double
+                    else if (guess == "2")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You have selected to bet on a not double!");
+                        RollAndDraw(die1, die2);
+                        Console.WriteLine($"{die1.Roll} and {die2.Roll}");
+                        //checking if not double
+                        if (die1.Roll != die2.Roll)
+                        {
+                            Console.WriteLine("You won " + bet / 2 + "$!");
+                            balance += bet / 2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lost " + bet + "$!");
+                        }
+                        Console.WriteLine("Press ENTER to continue.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        Betting = false;
+                    }
+                    //even sum
+                    else if (guess == "3")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You have selected to bet on an even sum!");
+                        RollAndDraw(die1, die2);
+                        Console.WriteLine($"{die1.Roll} and {die2.Roll}");
+                        //checking if even sum
+                        if ((die1.Roll + die2.Roll) % 2 == 0)
+                        {
+                            Console.WriteLine("You won " + bet + "$!");
+                            balance += bet;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lost " + bet + "$!");
+                        }
+                        Console.WriteLine("Press ENTER to continue.");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+
+                    //odd sum
+                    else if (guess == "4")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You have selected to bet on an odd sum!");
+                        RollAndDraw(die1, die2);
+                        Console.WriteLine($"{die1.Roll} and {die2.Roll}");
+                        //checking if odd sum
+                        if ((die1.Roll + die2.Roll) % 2 == 1)
+                        {
+                            Console.WriteLine("You won " + bet + "$!");
+                            balance += bet;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lost " + bet + "$!");
+                        }
+                        Console.WriteLine("Press ENTER to continue.");
+                        Console.ReadLine();
+
+
+
+
+
+                    }
+
                 }
-
-
-
-
-
             }
-
         }
-        public static void RollAndDraw(Die die1, Die die2) 
+        public static void RollAndDraw(Die die1, Die die2)
         {
             die1.RollDie();
             die2.RollDie();
